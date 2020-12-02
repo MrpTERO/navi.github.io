57ac4fbf1a640b25ef27504383e05ae3195d4535
MrPetro <v.gostax.u.Frosta@gmail.com>
2020-12-02 14:55:27 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+require_once "connect.php";
+
+if(empty($_GET['ln'])) {$pageLang='en';}
+    else {$pageLang=$_GET['ln'];}
+    
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    
+    <title>NaVi</title>
+<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="album.css" rel="stylesheet">
+  </head>
+  <body class="container">
+    <header>
+  <?php include "modules/header.php"; ?>
+  </header>
+
+<main role="main">
+
+  <?php
+  	include "modules/home.php";
+  	include "modules/cards.php";
+  	include "modules/form.php";
+  ?>
+
+  
+
+</main>
+
+<footer class="text-muted">
+  <?php include "modules/footer.php";  ?>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="../assets/dist/js/bootstrap.bundle.js"></script></body>
+</html>
